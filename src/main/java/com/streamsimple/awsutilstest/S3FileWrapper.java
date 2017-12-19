781e59e03b789ce4b9d86d380c17d3ca2625498e
Timothy Farkas <tim@streamsimple.io>
2017-12-18 22:21:29 -0800

    - Added license headers  - Added maven plugins to pom  - Fixed checkstyle errors

--- src/main/java/com/streamsimple/awsutilstest/S3FileWrapper.java
+++ src/main/java/com/streamsimple/awsutilstest/S3FileWrapper.java
@@ -1,7 +1,25 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.streamsimple.awsutilstest;
 
-import com.streamsimple.awsutils.S3Utils;
 import java.io.File;
+
+import com.streamsimple.awsutils.S3Utils;
 
 public class S3FileWrapper
 {
@@ -13,11 +31,11 @@ public class S3FileWrapper
   private File localFile;
 
   private S3FileWrapper(String awsAccessKey,
-                       String awsSecretKey,
-                       String region,
-                       String bucket,
-                       String objectName,
-                       File localFile)
+      String awsSecretKey,
+      String region,
+      String bucket,
+      String objectName,
+      File localFile)
   {
     this.awsAccessKey = awsAccessKey;
     this.awsSecretKey = awsSecretKey;
